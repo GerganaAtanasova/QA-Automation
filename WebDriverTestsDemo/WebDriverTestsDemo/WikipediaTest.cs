d350bf62706aaaa483a8a336bd1d0723aac1ae17
GerganaAtanasova <gergana.atanasova3333@gmail.com>
2022-05-22 18:44:39 +0300

    add new solution WebDriverTest

---
+++ WebDriverTestsDemo/WebDriverTestsDemo/WikipediaTest.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var driver = new ChromeDriver();
+
+//Navigate to wikipedia
+driver.Url = "https://wikipedia.org";
+
+Console.WriteLine("Current title: " + driver.Title);
+
+//locate field by id
+var searchField = driver.FindElement(By.Id("searchInput"));
+
+//click on element
+searchField.Click();
+
+//fill QA and press enter
+searchField.SendKeys("QA" + Keys.Enter);
+
+Console.WriteLine("Title after search: " + driver.Title);
+
+
+
+driver.Quit();
