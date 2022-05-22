b436627f46f94d2323dce5dc7b865faf5347eb65
GerganaAtanasova <gergana.atanasova3333@gmail.com>
2022-05-22 20:30:39 +0300

    change tests logic

--- WebDriverTestsDemo/NUnitWebDriverTests/SoftuniTests.cs
+++ WebDriverTestsDemo/NUnitWebDriverTests/SoftuniTests.cs
@@ -6,41 +6,40 @@ namespace NUnitWebDriverTests
 {
     public class SoftuniTests
     {
-
+        private WebDriver driver;
 
         [SetUp]
-        public void Setup()
+        public void OpenBrowserAndNavigate()
         {
-            
+            this.driver = new ChromeDriver();
+            driver.Url = "https://softuni.bg";
+            driver.Manage().Window.Maximize();
         }
+
+        [TearDown]
+        public void ShutDown()
+        {
+            driver.Quit();
+        }
+
 
         [Test]
         public void Test_AssertMainPageTitle()
         {
-            var driver = new ChromeDriver();
-            driver.Url = "https://softuni.bg";
             string expectedTitle = "Обучение по програмиране - Софтуерен университет";
 
             Assert.AreEqual(expectedTitle, driver.Title);
-
-            driver.Quit();
         }
 
         [Test]
         public void Test_AssertAboutUsTitle()
         {
-            var driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-
-            driver.Url = "https://softuni.bg";
             var zaNasElement = driver.FindElement(By.CssSelector("#header-nav > div.toggle-nav.toggle-holder > ul > li:nth-child(1) > a > span"));
             zaNasElement.Click();
 
             string expectedTitle = "За нас - Софтуерен университет";
 
             Assert.AreEqual(expectedTitle, driver.Title);
-
-            driver.Quit();
         }
     }
 }
